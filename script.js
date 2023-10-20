1657d3c849b94be8fd7b4068ea1abe95e54bab47
James Moses <jamesmoses262@gmail.com>
2023-10-20 18:37:08 +0100

    Hero section updated desktop view

--- script.js
+++ script.js
@@ -2,8 +2,28 @@
 
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".navMenu");
+const brandsText = document.querySelector(".BrandsText");
+const potentialText = document.querySelector(".PotentialText");
+const brandsHighlight = document.querySelector(".brandsHL");
+const potentialHighlight = document.querySelector(".potentialHL");
 
 hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('rotate');
     navMenu.classList.toggle('showNav');
+})
+
+brandsText.addEventListener("mouseover", () => {
+    brandsHighlight.classList.add("hlHeight");
+})
+
+brandsText.addEventListener("mouseout", () => {
+    brandsHighlight.classList.remove("hlHeight");
+})
+
+potentialText.addEventListener("mouseover", () => {
+    potentialHighlight.classList.add("hlHeight");
+})
+
+potentialText.addEventListener("mouseout", () => {
+    potentialHighlight.classList.remove("hlHeight");
 })
