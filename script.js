0c75e14200af415656896af90da81586660d9357
James Moses <jamesmoses262@gmail.com>
2023-10-27 11:06:59 +0100

    Services Section: First Update

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ const potentialHighlight = document.querySelector(".potentialHL");
 const activeNavBackground = document.querySelector(".navMenuActive");
 const scrollToAbtSec = document.querySelector(".downIcon");
 const abtUsSection = document.querySelector(".aboutUs");
+const mediaPlacementBtn = document.getElementById("mediaPlcment");
+const moreMediaPlacement = document.querySelector(".mediaPlacement");
+const closeMediaPlacement = document.getElementById("closeMediaPlacement");
 
 document.querySelector(".burgerContainer").addEventListener("click", () => {
     document.querySelector(".up").classList.toggle("uprotate");
@@ -33,4 +36,18 @@ potentialText.addEventListener("mouseover", () => {
 
 potentialText.addEventListener("mouseout", () => {
     potentialHighlight.classList.remove("hlHeight");
+})
+
+mediaPlacementBtn.addEventListener("click", () => {
+    moreMediaPlacement.classList.remove("mediaPlacement");   
+})
+
+closeMediaPlacement.addEventListener("click", () => {
+    moreMediaPlacement.classList.add("mediaPlacement");
+})
+
+const line1 = document.querySelector("#line1");
+
+line1.addEventListener('mouseover', () => {
+    line1.classList.add("rotate")
 })
